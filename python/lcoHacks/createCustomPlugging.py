56274c56af3048e620b227e1bd16728429b09e46
José Sánchez-Gallego <gallegoj@uw.edu>
2016-05-14 15:30:20 -0300

    initial commit

---
+++ python/lcoHacks/createCustomPlugging.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# createCustomPlugging.py
+#
+# Created by José Sánchez-Gallego on 14 May 2016.
+# Licensed under a 3-clause BSD license.
+#
+# Revision history:
+#    14 May 2016 J. Sánchez-Gallego
+#       Initial version
+
+
+from __future__ import division
+from __future__ import print_function
